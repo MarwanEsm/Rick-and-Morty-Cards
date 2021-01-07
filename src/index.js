f05eefcd64543e92e97bfaf9900a4ff50fc138e4
Marwan <marwan.esmaail85@gmail.com>
2021-01-07 16:52:01 +0100

    Add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React, { useState } from 'react';
 // import SearchBar from "./components/searchBar";
-// import FrontSide from "./components/frontSide";
-// import BackSide from "./components/backSide";
+import FrontSide from "./components/frontSide";
+import BackSide from "./components/backSide";
+import ReactCardFlip from "react-card-flip";
 // import FlipCard from "./components/flipcard";
 // import FetchData from "./components/fetchData";
 
@@ -15,42 +16,19 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-class BackSide extends Component {
-  constructor(props) {
-    super(props)
-    this.handleClick=this.handleClick.bind(this)
-    state = {
-      isLoading: true,
-      characterName: null,
-    };
-  }
-   /*handleClick(){} use this function to display a bigger image with more details*/
-  /* displays name etc of a specific characte, should we do a loop?*/
+const CardFlip = () => {
+  const [isFlipped, setIsFlipped] = useState(fals);
+  const handleClick = () => {
+    this.setIsFlipped(!isFlipped);
+  };
+  return (
+    <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
+      <FrontSide />
+      <button onClick={this.handleClick} />
+      <BackSide />
 
-  async componentDidMount() {
-    const url = "https://rickandmortyapi.com/api/character/";
-    const response = await fetch(url);
-    const data = await response.json();
-    this.setState({
-      characterName: data.results[0].name,
-      isLoading: false,
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h3>{this.state.characterName}</h3>
-        <a href='#' onClick={this.handleClick}>Show More</a>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<BackSide />, document.getElementById("root"));
-// ========================================
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <button onClick={this.handleClick} />
+    </ReactCardFlip>
+  );
+};
+ReactDOM.render(<FlipCard />, document.getElementById("root"));
