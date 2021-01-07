9f02a2163e6c1a7e4a57ce657b9a8e10e0e1412d
Marwan <marwan.esmaail85@gmail.com>
2021-01-07 14:32:48 +0100

    Add

--- src/compononts/fetchDataComponot.js
+++ src/compononts/fetchDataComponot.js
@@ -1,44 +1,44 @@
-// import React, { Component } from "react";
-// import ReactDOM from "react-dom";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
-// class FetchData extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       isLoading: true,
-//       dataSource: null,
-//     };
-//   }
-//   componentDidMount() {
-//     fetch("https://rickandmortyapi.com/api/character/")
-//       .then((response) => response.json())
-//       .then((responseJson) => {
-//         this.setState({
-//           isLoading: false,
-//           dataSource: responseJson.results,
-//         });
-//       })
+class FetchData extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      dataSource: null,
+    };
+  }
+  componentDidMount() {
+    fetch("https://rickandmortyapi.com/api/character/")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.results,
+        });
+      })
 
-//       .catch((error) => {
-//         return <h1>{error}</h1>;
-//       });
-//   }
+      .catch((error) => {
+        return <h1>{error}</h1>;
+      });
+  }
 
-//   render() {
-//     return (
-//       <div>
-//         <PageStyle />
-//         <FlipCard />
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <PageStyle />
+        <FlipCard />
+      </div>
+    );
+  }
+}
