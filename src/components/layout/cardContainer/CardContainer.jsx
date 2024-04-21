62207299e007d160923a8124e7b2a1a5bd626020
marwanEsm <marwan.esmaail85@gmail.com>
2024-04-21 15:43:30 +0200

    add flip

--- src/components/layout/cardContainer/CardContainer.jsx
+++ src/components/layout/cardContainer/CardContainer.jsx
@@ -10,27 +10,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const CardContainer = ({ character, onCardSelect, index, isSelected }) => {
 
     return <Col lg={3} md={6} sm={10} xs={14} className={styles.container}>
-        {
-            isSelected ?
-                <Card
-                    border="primary"
-                    className={classNames("p-3 card", styles.card)}
-                    onClick={() => onCardSelect(index)}
-                // onMouseLeave={() => onCardSelect(index)}
-                >
-                    <MoreInformation character={character} />
-                </Card>
-                :
-                <Card
-                    border="primary"
-                    className={classNames("p-3 card", styles.card)}
-                    onClick={() => onCardSelect(index)}
-                >
+        <Card
+            border="primary"
+            className={classNames("p-3 card", styles.card, { [styles.flip]: isSelected })}
+            onClick={() => onCardSelect(index)}
+        // onMouseLeave={() => onCardSelect(null)}
+        >
+            {isSelected ? (
+                <MoreInformation character={character} />
+            ) : (
+                <>
                     <Card.Img src={character.image} alt={`${character.name}`} />
                     <Card.Text className={styles.character_name}>{character.name}</Card.Text>
-                </Card>
-
-        }
+                </>
+            )}
+        </Card>
     </Col>
 }
 
