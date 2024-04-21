ef7e15fe5f74715df13426a9709f8d904502054a
marwanEsm <marwan.esmaail85@gmail.com>
2024-04-21 14:43:04 +0200

    modify style

--- src/components/layout/cardContainer/CardContainer.jsx
+++ src/components/layout/cardContainer/CardContainer.jsx
@@ -9,33 +9,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const CardContainer = ({ character, onCardSelect, index, isSelected }) => {
 
-    return <>
+    return <Col lg={3} md={6} sm={10} xs={14} className={styles.container}>
         {
             isSelected ?
-                <Col lg={3} md={6} sm={10} xs={14} className={styles.container}>
-                    <Card
-                        border="primary"
-                        className={classNames("p-3 card", styles.card)}
-                        onClick={() => onCardSelect(index)}
-                    // onMouseLeave={() => onCardSelect(index)}
-                    >
-                        <MoreInformation character={character} />
-                    </Card>
-                </Col>
-
+                <Card
+                    border="primary"
+                    className={classNames("p-3 card", styles.card)}
+                    onClick={() => onCardSelect(index)}
+                // onMouseLeave={() => onCardSelect(index)}
+                >
+                    <MoreInformation character={character} />
+                </Card>
                 :
-                <Col lg={3} md={6} sm={10} xs={14} className={styles.container}>
-                    <Card
-                        border="primary"
-                        className={classNames("p-3 card", styles.card)}
-                        onClick={() => onCardSelect(index)}
-                    >
-                        <Card.Img src={character.image} alt={`${character.name}`} />
-                        <Card.Text className={styles.character_name}>{character.name}</Card.Text>
-                    </Card>
-                </Col>
+                <Card
+                    border="primary"
+                    className={classNames("p-3 card", styles.card)}
+                    onClick={() => onCardSelect(index)}
+                >
+                    <Card.Img src={character.image} alt={`${character.name}`} />
+                    <Card.Text className={styles.character_name}>{character.name}</Card.Text>
+                </Card>
+
         }
-    </>
+    </Col>
 }
 
 
